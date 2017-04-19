fa481be3c8edd9b8dbad34dd77f8a8ecb464bc0b
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2017-04-19 11:27:51 +0200

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+const ok = function (req, res) {
+  return res.send('ok')
+}
+const r = function (req, res) {
+  res.json({ url: req.url, body : req.body });
+}
+app.get('*', r);
+app.get('/ok', ok);
+app.post('*', r);
+
+app.listen(8000, function () {
+  console.log('Listening on 8000')
+});
